626dca1d7d7542ac7752f647d31a8201e2c1cb7b
kuppi0315-rgb <kuppi0315@gmail.com>
2026-05-12 09:38:59 +0900

    Update ranking.js

--- scripts/ranking.js
+++ scripts/ranking.js
@@ -1,1 +1,21 @@
+const data = [
+  { name: "You", pomodoro: localStorage.getItem("total") || 0 },
+  { name: "Friend A", pomodoro: 42 },
+  { name: "Friend B", pomodoro: 30 },
+  { name: "Friend C", pomodoro: 18 }
+];
 
+data.sort((a,b)=>b.pomodoro - a.pomodoro);
+
+const list = document.getElementById("rank-list");
+
+data.forEach((d,i)=>{
+
+  const div = document.createElement("div");
+  div.className = "rank-item";
+
+  div.innerText = `${i+1}位 ${d.name} : ${d.pomodoro}回`;
+
+  list.appendChild(div);
+
+});
